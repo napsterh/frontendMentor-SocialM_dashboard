b48629e3f69fda7e39af581825e425b3ffeecf6f
Napster <rome.1205h@gmail.com>
2020-05-24 23:12:08 -0500

    add hooks para modo dark

--- src/switch.js
+++ src/switch.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './switch.css'
 
 function Switch() {
+
+    const ref = useRef(null)
+    function handleChange(event){
+        console.log(ref.current.checked);
+        if(ref.current.checked){
+            document.body.classList.remove('is-light-mode')
+            document.body.classList.add('is-dark-mode')
+        } else {
+            document.body.classList.remove('is-dark-mode')
+            document.body.classList.add('is-light-mode')
+        }
+    }
+
     return (
         <div className="dark-mode">
             <p className="dark-mode-title">Dark Mode</p>
-                <input type="checkbox" className="checkbox" id="checkbox"/>
+                <input ref={ref} onChange={handleChange} type="checkbox" className="checkbox" id="checkbox"/>
                 <label className="switch" htmlFor="checkbox">
 
                 </label>
